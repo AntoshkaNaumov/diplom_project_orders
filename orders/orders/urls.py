a9b094f4639829bf0dfbd0ce5eff69ddde0b153c
Anton Naumov <anton.naumov@inbox.ru>
2023-02-02 20:05:15 +0300

    First commit

---
+++ orders/orders/urls.py
@@ -0,0 +1,40 @@
+"""orders URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from api.views import *
+from django_rest_passwordreset.views import reset_password_confirm, reset_password_request_token
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/register', RegisterUser.as_view()),
+    path('user/register/confirm', Сonfirmation.as_view()),
+    path('user/login', LoginUser.as_view()),
+    path('user/contact', ContactView.as_view()),
+    path('user/contacts', ContactAPIList.as_view()),
+    path('user/details', DetailUser.as_view()),
+    path('user/passwordreset', reset_password_request_token),
+    path('user/passwordreset/confirm', reset_password_confirm),
+    path('shops', ShopView.as_view()),
+    path('categories', CategoryView.as_view()),
+    path('products', ProductView.as_view()),
+    path('partner/update', PartnerUpdate.as_view()),
+    path('partner/state', PartnerState.as_view()),
+    path('partner/orders', PartnerOrders.as_view()),
+    path('cart', CartView.as_view()),
+    path('order', OrderView.as_view()),
+]
